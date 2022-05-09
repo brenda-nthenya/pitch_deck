6172f76d939cc4f18e0eca65a0b965317d9a171f
Brenda <bee@bee.com>
2022-05-09 16:23:50 +0300

    Add the user model

--- app/models.py
+++ app/models.py
@@ -5,6 +5,16 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255))
+    comment_id = db.Column(db.Integer,db.ForeignKey('comment.id'))
 
     def __repr__ (self):
         return f'User {self.username}'
+
+class Comments(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.name}'
