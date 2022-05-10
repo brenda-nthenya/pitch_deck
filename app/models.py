d1525a439b15c8cc25c3581fbcda8066d524b696
Brenda <bee@bee.com>
2022-05-10 11:49:04 +0300

    Updated User Module

--- app/models.py
+++ app/models.py
@@ -1,20 +1,34 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255))
-    comment_id = db.Column(db.Integer,db.ForeignKey('comment.id'))
+    pass_secure = db.Column(db.String(255))
 
-    def __repr__ (self):
+    @property
+    def  password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self,password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_passowrd(self, password):
+        return check_password_hash(self.pass_secure,password)
+
+
+
+    def __repr__(self):
         return f'User {self.username}'
-
 class Comments(db.Model):
     __tablename__ = 'comments'
 
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(255))
+    comment = db.Column(db.String(), nullable = False)
 
     def __repr__(self):
         return f'User {self.name}'
