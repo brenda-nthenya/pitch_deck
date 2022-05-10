12567e894412fe7e28d955a511eacc843da5b5c1
Brenda <bee@bee.com>
2022-05-10 15:39:26 +0300

    Adding more columns to user table

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from . import db,login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-
+from datetime import datetime
 
 
 class User(UserMixin,db.Model):
@@ -11,6 +11,12 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique=True,index=True)
     pass_secure = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
+    upvote = db.relationship('Upvote',backref='user',lazy='dynamic')
+    downvote = db.relationship('Downvote',backref='user',lazy='dynamic')
 
 
     def save_u(self):
@@ -88,8 +94,28 @@ class Downvote(db.Model):
     def __repr__(self):
         return f'{self.user_id}:{self.pitch_id}'
 
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255),nullable = False)
+    post = db.Column(db.Text(), nullable = False)
+    comment = db.relationship('Comment',backref='pitch',lazy='dynamic')
+    upvote = db.relationship('Upvote',backref='pitch',lazy='dynamic')
+    downvote = db.relationship('Downvote',backref='pitch',lazy='dynamic')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    time = db.Column(db.DateTime, default = datetime.utcnow)
+    category = db.Column(db.String(255), index = True,nullable = False)
+    
+    def save_p(self):
+        db.session.add(self)
+        db.session.commit()
 
-@login_manager
+        
+    def __repr__(self):
+        return f'Pitch {self.post}'
+
+
+@login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
