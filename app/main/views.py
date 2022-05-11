02469c6c59ff04cf8efb28653dd0387c0286955c
Brenda <bee@bee.com>
2022-05-11 10:39:36 +0300

    Adds th update_pic function

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,8 @@
 from flask import render_template,redirect,request,url_for, abort
-
+from ..models import *
 from flask_login import login_required
 from . import main
+from .. import db,photos
 
 @main.route('/')
 def index():
@@ -9,4 +10,13 @@ def index():
     title = 'PITCH YOUR IDEAS'
     return render_template('index.html', title = title)
 
-
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
