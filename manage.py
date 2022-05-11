3ce23017c1fe8c63a4c19d2bbc9ae782ff074b5f
Brenda <bee@bee.com>
2022-05-11 12:16:37 +0300

    Adds Flaks Migrate and Shell

--- manage.py
+++ manage.py
@@ -1,12 +1,26 @@
 from app import create_app, db
-from app.models import User
-from flask_script import Manager, Server
+from app.models import User,Comments
+from flask_script import Manager,Shell, Server
+from flask_migrate import Migrate, MigrateCommand
 
 # Create app instance
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server', Server)
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Comments=Comments)
+
+migrate = Migrate(app,db)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
