7e8bc0ca16cfbefbd9cc421a0921edef502cc012
Brenda <bee@bee.com>
2022-05-11 22:15:24 +0300

    Update of ProdConfig

--- config.py
+++ config.py
@@ -15,10 +15,6 @@ class Config:
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     
-    @staticmethod
-    def init_app(app):
-        pass
-    
 
 class TestConfig(Config):
     pass
