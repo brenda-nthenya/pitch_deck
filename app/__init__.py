4a71139881d174a1df40362c884bb062e827129b
Brenda <bee@bee.com>
2022-05-12 10:36:23 +0300

    Fixing deployment errors

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from config import config_options
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads, IMAGES
 from flask_mail import Mail
-from sqlalchemy.dialects import postgresql
+
 
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
